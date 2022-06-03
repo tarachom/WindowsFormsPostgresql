035415c2b701398a6db270e43526c8347dada56f
tarachom <tarachom@i.ua>
2022-06-03 19:12:32 +0300

    Add project files.

---
+++ WindowsFormsPostgresql/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace WindowsFormsPostgresql
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridViewRecords = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRecords)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(105, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Вставити";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridViewRecords
+            // 
+            this.dataGridViewRecords.AllowUserToAddRows = false;
+            this.dataGridViewRecords.AllowUserToDeleteRows = false;
+            this.dataGridViewRecords.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridViewRecords.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridViewRecords.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewRecords.Location = new System.Drawing.Point(12, 60);
+            this.dataGridViewRecords.Name = "dataGridViewRecords";
+            this.dataGridViewRecords.ReadOnly = true;
+            this.dataGridViewRecords.RowHeadersVisible = false;
+            this.dataGridViewRecords.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewRecords.Size = new System.Drawing.Size(841, 475);
+            this.dataGridViewRecords.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Обновити";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(265, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Видалити";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(865, 547);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridViewRecords);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRecords)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridViewRecords;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsPostgresql/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+using Npgsql;
+
+namespace WindowsFormsPostgresql
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+		private NpgsqlConnection Connection { get; set; }
+
+		private BindingList<Records> RecordsBindingList { get; set; }
+
+		//Підключення до сервера PostgreSQL
+		private void Open(string connectionString)
+		{
+			Connection = new NpgsqlConnection(connectionString);
+			Connection.Open();
+		}
+
+		private class Records
+		{
+			public Records(string _id, string _name)
+			{
+				ID = _id;
+				Name = _name;
+			}
+			public string ID { get; set; }
+			public string Name { get; set; }
+		}
+
+		//Завантаження даних
+		private void LoadRecords()
+        {
+			RecordsBindingList.Clear();
+
+			string query = "SELECT * FROM public.tab1 ORDER BY id ASC";
+
+			NpgsqlCommand nCommand = new NpgsqlCommand(query, Connection);
+
+			NpgsqlDataReader reader = nCommand.ExecuteReader();
+
+			while (reader.Read())
+			{
+				RecordsBindingList.Add(new Records(
+					reader["id"].ToString(),
+					reader["name"].ToString()
+					));
+			}
+
+			reader.Close();
+		}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+			RecordsBindingList = new BindingList<Records>();
+			dataGridViewRecords.DataSource = RecordsBindingList;
+
+			dataGridViewRecords.Columns["Name"].Width = 300;
+
+			//Підключення до сервера PostgreSQL
+			Open($"Server=localhost;User Id=postgres;Password=1;Port=5433;Database=test;");
+
+			//Заватаження даних з таблиці
+			LoadRecords();
+		}
+
+		//Новий запис
+        private void button1_Click(object sender, EventArgs e)
+        {
+			string Name = new Random().Next(int.MaxValue).ToString();
+
+			string query = $"INSERT INTO public.tab1(name) VALUES('Назва {Name}')";
+
+			NpgsqlCommand nCommand = new NpgsqlCommand(query, Connection);
+			nCommand.ExecuteNonQuery();
+
+			//Заватаження даних з таблиці
+			LoadRecords();
+		}
+
+		//Обновлення таблиці
+        private void button2_Click(object sender, EventArgs e)
+        {
+			//Заватаження даних з таблиці
+			LoadRecords();
+		}
+
+		//Видалення рядочків
+        private void button3_Click(object sender, EventArgs e)
+        {
+			if (dataGridViewRecords.SelectedRows.Count != 0 &&
+				MessageBox.Show("Видалити записи?", "Повідомлення", MessageBoxButtons.YesNo) == DialogResult.Yes)
+			{
+				string query = "DELETE FROM public.tab1 WHERE id = @id";
+
+				NpgsqlCommand nCommand = new NpgsqlCommand(query, Connection);
+				
+				for (int i = 0; i < dataGridViewRecords.SelectedRows.Count; i++)
+				{
+					DataGridViewRow row = dataGridViewRecords.SelectedRows[i];
+					string id = row.Cells[0].Value.ToString();
+
+					if (!nCommand.Parameters.Contains("id"))
+						nCommand.Parameters.Add(new NpgsqlParameter("id", int.Parse(id)));
+					else
+						nCommand.Parameters["id"].Value = int.Parse(id);
+
+					nCommand.ExecuteNonQuery();
+				}
+
+				LoadRecords();
+			}
+		}
+
+
+    }
+}
